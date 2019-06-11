5e831a2d22e34a0e960b0d663e631b85988f58fd
KatieTooher <katie.tooher@marquette.edu>
2019-06-11 18:33:02 -0500

    adding file project template and text file

---
+++ fileprojtemplate.py
@@ -0,0 +1,38 @@
+##This is a template for the file reading project 
+
+class Character:
+    
+    def __init__(self,name,durability,strength,jump):
+        self.name = name
+        self.durability = durability
+        self.strength = strength
+        self.jump = jump
+
+
+    def __str__(self):
+        ##write the function to print out your character objects
+
+
+
+
+def add_char(name, dur, stren, jump):
+    ##write a function that would allow you to add a character to the file
+    ##parameters are the sections to add to the file
+    ##try this function last 
+    pass
+
+def main():
+
+    #file reading goes here
+    ##use the split() function
+    
+   
+
+main()
+    ##create a list that will hold all the character objects you create
+    ##using a loop and file reading
+
+
+    #sort the characters based on their durability
+
+    
